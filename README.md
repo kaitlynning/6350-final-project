da98612a2b90e946bca30ce770fbb902a1c1acd4
kaitlynning <kaitlynning88308@gmail.com>
2021-04-16 01:43:02 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# exercise_5
+
+Flutter/ Dart/ Firebase
+
+a  Create flutter login demo
+
+b  Set email/password to login.
+
+c If verified, user can login successfully.
+
+d After done checking, click right top button to logout.
+
+# class-exercise-5a-kaitlynning
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'authentication.dart';
+import 'login_signup_page.dart';
+import 'root_page.dart';
+
+void main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+        title: 'Flutter login demo',
+        debugShowCheckedModeBanner: false,
+        theme: new ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: new RootPage(auth: new Auth()));
+  }
+}
